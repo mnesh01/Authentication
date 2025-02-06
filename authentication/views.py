b7dd16f527a33bfbaee00ded499091055a6fa7b9
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-06 13:49:27 +0300

    add authentication view for signup

--- authentication/views.py
+++ authentication/views.py
@@ -3,14 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hello I am working')
+    return render(request, "authentication/index.html")
 
 def signup(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+             
     return render(request, "authentication/signup.html")
 
 def signin(request):
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    return render(request, "authentication/signout.html")
+    pass
  
