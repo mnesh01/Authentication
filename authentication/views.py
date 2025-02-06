53cfc2933bad028ba45e85264a8b4188a6c5b640
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-06 14:04:58 +0300

    success message and migrations

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -16,11 +17,14 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
         
-        myuser = User.objects.create(username, email, pass1)
+        myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         
         myuser.save()
+        
+        messages.success(request, "Account Successfully created")
+        return redirect('signin')
              
     return render(request, "authentication/signup.html")
 
