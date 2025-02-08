eedab040cc68b1897ffc6d7d4d990e098f1cb78c
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-08 14:27:00 +0300

    add app pass

--- login/info.py
+++ login/info.py
@@ -4,5 +4,5 @@ import os
 EMAIL_USE_TLS = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_HOST_USER = 'mnesh002@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Fetch from environment variable
+EMAIL_HOST_PASSWORD = 'bnas sczt ctis ttxx'  # Fetch from environment variable
 EMAIL_PORT = 587
