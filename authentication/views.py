c1c7c217e72d516565b99852f2e4d569968ff3ef
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-08 22:14:19 +0300

    Enhance email confirmation and user activation flow

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import generate_token
+from django.core.mail import EmailMessage, send_mail
+from django.contrib.sites.shortcuts import get_current_site
 
 def home(request):
     return render(request, "authentication/index.html")
@@ -40,9 +47,9 @@ def signup(request):
         myuser = User.objects.create_user(username=username, email=email, password=pass1)
         myuser.first_name = fname
         myuser.last_name = lname
+        myuser.is_active = False
         myuser.save()
-
-        messages.success(request, "Your account has been successfully created.")
+        messages.success(request, "Your account has been successfully created.Please confirm your email to activate your account")
         
         #Welcome email
         subject = "Welcome to Django login system"
@@ -50,6 +57,26 @@ def signup(request):
         from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+        #Confirmation email
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email address"
+        message2 = render_to_string('confirm_email.html',{
+            'name' : myuser.first_name,
+            'domain' : current_site.domain,
+            'uid' : urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token' : generate_token.make_token(myuser)
+        })
+        
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+        
    
         return redirect('signin')
 
@@ -77,3 +104,20 @@ def signout(request):
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect('home')
+
+
+def activate(request, uidb64,token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+        
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login (request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_fail.html')
+    
